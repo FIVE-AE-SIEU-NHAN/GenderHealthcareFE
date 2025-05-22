c777ab06278f793042cf6f414f9f97d508cb2b62
Nguyen Dinh Hung <biga5102@gmail.com>
2025-05-22 21:11:32 +0700

    Add Booking Services Page

---
+++ src/Application/BookingService.tsx
@@ -0,0 +1,28 @@
+import BookingForm from "./BookingForm";
+
+const BookingService: React.FC = () => {
+  return (
+    <div>
+      <div className="mb-15">
+                {/* Banner */}
+                <section className='bg-blue-100 py-0 text-center'>
+                    <div className='relative w-full'>
+                      <img src='/images/banner_blog.png' alt='' className='rounded w-full' />
+                      <div className='absolute inset-0 w-full bg-[#1A2159]/70 flex flex-col items-center justify-center'>
+                        <h2 className='text-6xl font-bold text-white'>Our Blogs</h2>
+                        <p className='text-sm mt-3'>
+                          <span className='text-white'>Home</span>
+                          <span className='text-[#55AEFF]'> &gt; BookingForms</span>
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+
+      </div>
+      <div className="mb-15"><BookingForm /></div>
+      
+    </div>
+  );
+};
+
+export default BookingService;
