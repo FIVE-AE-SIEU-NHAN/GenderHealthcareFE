64f420062458bd6467f503ec7741e66d3cd6ba8c
Nguyen Dinh Hung <biga5102@gmail.com>
2025-06-02 15:31:49 +0700

    Update BookingService.tsx

--- src/Application/BookingService.tsx
+++ src/Application/BookingService.tsx
@@ -9,7 +9,7 @@ const BookingService: React.FC = () => {
                     <div className='relative w-full'>
                       <img src='/images/banner_blog.png' alt='' className='rounded w-full' />
                       <div className='absolute inset-0 w-full bg-[#1A2159]/70 flex flex-col items-center justify-center'>
-                        <h2 className='text-6xl font-bold text-white'>Our Blogs</h2>
+                        <h2 className='text-6xl font-bold text-white'>BookingForms</h2>
                         <p className='text-sm mt-3'>
                           <span className='text-white'>Home</span>
                           <span className='text-[#55AEFF]'> &gt; BookingForms</span>
