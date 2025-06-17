60425d9fea43b9e687e2a108fd00bf24070210cb
redrag2105 <akiettran2005@gmail.com>
2025-06-18 02:49:39 +0700

    Add Google Auth

--- src/pages/Auth/GoogleLogin.tsx
+++ src/pages/Auth/GoogleLogin.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { GoogleCredentialResponse, GoogleLogin } from "@react-oauth/google";
 import googleLogo from '@/assets/images/google.png';
+import { authApi } from '@/apis/authApi';
+// import { toast } from 'sonner';
+import { useAuth } from '@/contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 
 export default function GoogleLoginButton() {
-  const handleSuccess = (credentialResponse: GoogleCredentialResponse) => {
+  const { login: loginContext } = useAuth();
+  const navigate = useNavigate();
+  const handleSuccess = async (credentialResponse: GoogleCredentialResponse) => {
     console.log("Google login success:", credentialResponse);
+    const data = {
+      id_token: credentialResponse.credential,
+    } as { id_token: string };
+    
+    if (credentialResponse.credential) {
+      const res = await authApi.loginWithGoogle(data);
+      const accessToken = res?.data?.result?.access_token;
+
+      if ([200,201].includes(res.status)) {
+        loginContext(accessToken);
+        toast.success("Login with Google", 
+          {
+            description: res.status === 200 ? "Welcome back!" : "Account created successfully!",
+          });
+        navigate('/');
+      }
+    }
   }
+
   return (
     <div className="relative h-[45px] w-full flex">
       <button
