18fd83c1c40f2b2457171992230a8e1976abfd99
Nguyen Dinh Hung <biga5102@gmail.com>
2025-07-01 23:39:03 +0700

    fix link

--- src/apis/customer/blogApi.ts
+++ src/apis/customer/blogApi.ts
@@ -14,7 +14,7 @@ export const fetchBlogDetail = async (blogId: string): Promise<Blog> => {
   // 3. Sử dụng axiosInstance để gọi API. 
   // Bạn không cần phải lo về base URL, content-type, hay access token
   // vì tất cả đã được xử lý trong `axiosConfig.ts`.
-  const response = await api.get<BackendBlogDetailResponse>(`/blog/customer/${blogId}`);
+  const response = await api.get<BackendBlogDetailResponse>(`/blog/detail/${blogId}`);
 
   // 4. API trả về dữ liệu blog trong `response.data.result.blog`.
   // Chúng ta trả về trực tiếp object `blog` này để hook Tanstack Query
