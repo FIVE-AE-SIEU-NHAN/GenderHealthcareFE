578f59ec6e55b2d1a85812cce3018e397ca4c36e
redrag2105 <akiettran2005@gmail.com>
2025-07-15 19:39:01 +0700

    Add env, rename some files

--- src/main.tsx
+++ src/main.tsx
@@ -8,19 +8,21 @@ import 'swiper/css/effect-fade';
 import 'sal.js/dist/sal.css';
 import './index.css'
 
-import ENV from './utils/environment';
 import AppRouter from './Application/router/AppRouter';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/AuthContext';
+import { SocketProvider } from './contexts/SocketContext';
 
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <GoogleOAuthProvider clientId={ENV.GG_CLIENT_ID}>
-        <AppRouter />
-      </GoogleOAuthProvider>
+      <SocketProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GG_CLIENT_ID}>
+          <AppRouter />
+        </GoogleOAuthProvider>
+      </SocketProvider>
     </AuthProvider>
   </QueryClientProvider>
   // </StrictMode>
--- src/utils/environment.ts
+++
@@ -1,4 +0,0 @@
-const ENV = {
-  GG_CLIENT_ID : '341901301220-b5u6ijfcfjbrejjjjj3av5ncislncet7.apps.googleusercontent.com'
-}
-export default ENV;
